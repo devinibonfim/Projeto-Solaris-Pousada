16e0282dfa98a1e6eb381c1f77484702fa27c699
Dev0Nakayama <89699349+Dev0Nakayama@users.noreply.github.com>
2021-10-08 12:21:18 -0300

    Database(Not Found) change v1
    Update database in project, not faund

--- hotel/app/Models/tipoQuarto.php
+++ hotel/app/Models/tipoQuarto.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class associado extends Model
+class tipoQuarto extends Model
 {
     use HasFactory;
-    protected $filleable=[
-        'nome','idade','id_hospede',
-    ];
 }
--- hotel/database/migrations/2021_10_07_151734_create_tipo_quartos_table.php
+++ hotel/database/migrations/2021_10_07_151734_create_tipo_quartos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAssociadosTable extends Migration
+class CreateTipoQuartosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateAssociadosTable extends Migration
      */
     public function up()
     {
-        Schema::create('associados', function (Blueprint $table) {
+        Schema::create('tipo_quartos', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_hospede_associado');
-            $table->string('nomeAssociado');
-            $table->string('idadeAssociado');
             $table->timestamps();
-            $table->foreign('id_hospede_associado')->references('id')->on('hospedes');
         });
     }
 
@@ -30,6 +26,6 @@ class CreateAssociadosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('associados');
+        Schema::dropIfExists('tipo_quartos');
     }
 }
