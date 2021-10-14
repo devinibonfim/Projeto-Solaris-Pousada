ee978c56ada90bb01ad3f6269106bfbb43d47160
Vinícius Bonfim <84873383+devinibonfim@users.noreply.github.com>
2021-10-14 09:42:10 -0300

    v3.1.5
    - index

--- hotel/app/Models/Associado.php
+++ hotel/app/Models/Associado.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Associado extends Model
+class associado extends Model
 {
     use HasFactory;
+    protected $filleable=[
+        'nome','idade','id_hospede',
+    ];
 }
