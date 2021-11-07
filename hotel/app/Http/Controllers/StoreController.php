2907a01c88560417e1c6adfbc9f6f90d8345fd8a
Dev0Nakayama <89699349+Dev0Nakayama@users.noreply.github.com>
2021-11-07 12:06:12 -0300

    v3.5.5.2
    Update funcionando, Falta campo de endereço

--- hotel/app/Http/Controllers/StoreController.php
+++ hotel/app/Http/Controllers/StoreController.php
@@ -19,98 +19,29 @@ class StoreController extends Controller
 
     public function storeFuncionario(Request $request)
     {    
-
-        /** /
-        $user['name'] = $request->get('name');
-        $user['email'] = $request->get('email');
-        $user['password'] = $request->get('password');
-
-        $pessoa['telefone'] = $request->get('telefone');
-        $pessoa['nacionalidade'] = $request->get('nacionalidade');
-        $pessoa['dataNascimento'] = $request->get('dataNascimento');
-        
-        $funcionario['ra'] = $request->get('ra');
-        $funcionario['rg'] = $request->get('rg');
-        $funcionario['pis_pasep'] = $request->get('pis_pasep');
-     
-        User::create($user);
-        Pessoa::create($pessoa);
-        Funcionario::create($funcionario);
-        /**/
-
-        //
-
-        /** /
+        $user = new User;
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
         $user->save();
 
-        //KLUDGE!! rest of site depends on user id in auth object - need to force/create it here
-        Auth::attempt(array('email' => $request->input::get('email'), 'password' => $request->input::get('password')), true);
+        $userid  = $user->id;
+        $pessoa = new Pessoa;
+        $pessoa->user_id = $userid;
+        //falta endereço
+        $pessoa->telefone = $request->input('telefone');
+        $pessoa->data_nascimento = $request->input('data_nascimento');
+        $pessoa->nacionalidade = $request->input('nacionalidade');
+        $pessoa->save();
 
-        return redirect('users/' . Auth::user()->id);
-        /**/
+        $pessoaid  = $pessoa->id;
+        $funcionario = new Funcionario;
+        $funcionario->pessoa_id = $pessoaid;
+        $funcionario->ra = $request->input('ra');
+        $funcionario->rg = $request->input('rg');
+        $funcionario->pis_pasep = $request->input('pis_pasep');
+        $funcionario->save();
 
-        /** /
-        $user = Auth::user();
-        User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ]);
-
-
-
-        Pessoa::create([
-            'user_id'=>  User::findOrFail($id),
-            'telefone' => $request->input('telefone'),
-            'nacionalidade' => $request->input('nacionalidade'),
-            'data_nascimento' => $request->input('data_nascimento'),
-        ]);
-        Funcionario::create([
-            'pessoa_id'=> auth()->id(),
-            'ra' => $request->input('ra'),
-            'rg' => $request->input('rg'),
-            'pis_pasep' => $request->input('pis_pasep'),
-        ]);
-        /**/
-
-
-        /** /
-        //Atulizar usuario
-        User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ]);
-        Pessoa::create([
-            'user_id'=>  $this->attributes['user_id'] = Auth::user()->id,
-            'telefone' => $request->input('telefone'),
-            'nacionalidade' => $request->input('nacionalidade'),
-            'data_nascimento' => $request->input('data_nascimento'),
-        ]);
-        /**/
-
-        /**/
-        $funcioanrio = new Funcionario([
-            'ra' => $request->input('ra'),
-            'rg' => $request->input('rg'),
-            'pis_pasep' => $request->input('pis_pasep'),
-        ]);
-
-        $pessoa = new Pessoa([
-            'telefone' => $request->input('telefone'),
-            'nacionalidade' => $request->input('nacionalidade'),
-            'data_nascimento' => $request->input('data_nascimento'),
-        ]);
-
-        $user = new User([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ]);
-
-        $user->pessoa()->save($user);
-        /**/
-        dd();
-        //return redirect('/');
+        return redirect(route('FuncView'));
     }
 }
