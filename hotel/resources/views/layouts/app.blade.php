62a69424eddaf77511d93b4a5b4b71f7cbc8a77d
Vinicius Bonfim <devinibonfim@gmail.com>
2021-11-16 20:39:22 -0300

    v3.7.3
    - gambiarra perfil adm e funcionario

--- hotel/resources/views/layouts/app.blade.php
+++ hotel/resources/views/layouts/app.blade.php
@@ -136,12 +136,21 @@
                             <a id="navbarDropdown" class="nav-link dropdown-toggle nav-link-white text-capitalize" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
-
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                @if(Auth::user()->funcionario)
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                                @else
                                 <a class="dropdown-item" href="{{ route('perfil') }}">Perfil</a>
                                 <a class="dropdown-item" href="{{ route('reserva2') }}">Reservas</a>
                                 <a class="dropdown-item" href="{{ route('ajuda') }}">Ajuda</a>
-
+                                
                                 <hr>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -151,6 +160,7 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
+                                @endif
                             </div>
                         </li>
                         @if(Auth::user()->funcionario)
